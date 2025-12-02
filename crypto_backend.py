35f303fb2450730e17c3b89cc82cd3690f7b37ce
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-02 18:21:52 +0700

    Comment

--- crypto_backend.py
+++ crypto_backend.py
@@ -21,34 +21,44 @@ def derive_key(password: str, salt: bytes) -> bytes:
     return kdf.derive(password.encode())
 
 def encrypt_data(data: bytes, password: str) -> bytes:
+    #Generate Random Salt 
     salt = os.urandom(SALT_SIZE)
     iv = os.urandom(IV_SIZE)
 
     key = derive_key(password, salt)
 
+    #Cipher AES 256
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
 
+    #Padding PKCS7
     padder = padding.PKCS7(BLOCK_SIZE).padder()
     padded_data = padder.update(data) + padder.finalize()
 
+    # Encrypt Padded Data
     ciphertext = encryptor.update(padded_data) + encryptor.finalize()
 
+    # Merge Salt + IV + Ciphertext for Decryption
     return salt + iv + ciphertext
 
 def decrypt_data(encrypted_data: bytes, password: str) -> bytes:
     try:
+        # Extract salt + iv 
         salt = encrypted_data[:SALT_SIZE]
         iv = encrypted_data[SALT_SIZE : SALT_SIZE + IV_SIZE]
         ciphertext = encrypted_data[SALT_SIZE + IV_SIZE:]
 
+        # Derive key
         key = derive_key(password, salt)
 
+        # Decryptor
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
         decryptor = cipher.decryptor()
 
+        # Decrypt Data
         padded_data = decryptor.update(ciphertext) + decryptor.finalize()
 
+        # Unpadding
         unpadder = padding.PKCS7(BLOCK_SIZE).unpadder()
         original_data = unpadder.update(padded_data) + unpadder.finalize()
 
