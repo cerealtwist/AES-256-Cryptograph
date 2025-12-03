d1c3b0388cc8d80231a3aa2da9159b0f04a6655d
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-03 18:13:34 +0700

    Update dummy (for folder)

--- crypto_dummy.py
+++ crypto_dummy.py
@@ -1,11 +1,18 @@
 import os
 
-def create_dummy_csv(filename="test_dataset.csv"):
-    if not os.path.exists(filename):
-        print(f"[SETUP] Generating dummy file '{filename}' di {os.getcwd()}...")
-        with open(filename, "w") as f:
+def create_dummy_csv(filename="test_dataset.csv", folder="output_files"):
+    if not os.path.exists(folder):
+        print(f"[SETUP] Folder '{folder}' not found. Creating new folder...")
+        os.makedirs(folder)
+
+    # Merge folder name and file name (ex. output_files/test_dataset.csv)
+    full_path = os.path.join(folder, filename)
+
+    if not os.path.exists(full_path):
+        print(f"[SETUP] Generating dummy file '{full_path}' di {os.getcwd()}...")
+        with open(full_path, "w") as f:
             f.write("ID,Name,GPA,Subject\n")
             f.write("101,Faiz,3.5,Data Science\n")
             f.write("102,Gres,3.8,Informatika\n")
             f.write("103,Haikal,3.2,Sistem Informasi")
-    return filename
+    return full_path
